59080cecfc085c6ff15e8571a0ff6cc020501004
Diah Junaidi <j.fdiah@gmail.com>
2024-01-18 14:53:58 +0700

    update beberapa kebutuhan untuk authenticate

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth.controller');
+const { authenticateToken } = require('../middlewares/auth.middleware');
 
 // Register User
 router.post('/register', authController.registerUser);
 
 // Login User
 router.post('/login', authController.loginUser);
+
+// Secure Route - Hanya dapat diakses jika memiliki token JWT yang valid
+router.get('/secure', authenticateToken, authController.secureRoute);
 
 module.exports = router;
